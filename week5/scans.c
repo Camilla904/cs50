896dddc7ccc263a5a77338d207e9335cdc3c22a4
Camilla <64920167+Camilla904@users.noreply.github.com>
2020-12-03 10:19:43 -0500

    Create scans.c

---
+++ week5/scans.c
@@ -0,0 +1,61 @@
+// Gets an int from user using scanf
+
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    printf("x: ");
+    scanf("%i", &x);
+    printf("x: %i\n", x);
+}
+
+// Incorrectly gets a string from user using scanf
+
+#include <stdio.h>
+
+int main(void)
+{
+    char *s;
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+}
+
+// Dangerously gets a string from user using scanf
+
+#include <stdio.h>
+
+int main(void)
+{
+    char s[5];
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+}
+
+
+// Swaps two integers using pointers
+
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int main(void)
+{
+    int x = 1;
+    int y = 2;
+
+    printf("x is %i, y is %i\n", x, y);
+    swap(&x, &y);
+    printf("x is %i, y is %i\n", x, y);
+}
+
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+
