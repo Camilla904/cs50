1c2f8679f7d7401f45ef5be988e05c5c4bd5137b
Camilla <64920167+Camilla904@users.noreply.github.com>
2020-12-03 10:16:43 -0500

    Create copy.c

---
+++ week5/copy.c
@@ -0,0 +1,83 @@
+// Capitalizes a string
+
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Get a string
+    string s = get_string("s: ");
+
+    // Copy string's address
+    string t = s;
+
+    // Capitalize first letter in string
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
+
+    // Print string twice
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+}
+
+// Capitalizes a copy of a string
+
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    // Get a string
+    char *s = get_string("s: ");
+
+    // Allocate memory for another string
+    char *t = malloc(strlen(s) + 1);
+
+    // Copy string into memory
+    for (int i = 0, n = strlen(s); i <= n; i++)
+    {
+        t[i] = s[i];
+    }
+
+    // Capitalize copy
+    t[0] = toupper(t[0]);
+
+    // Print strings
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+}
+
+// Capitalizes a copy of a string using strcpy
+
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    // Get a string
+    char *s = get_string("s: ");
+
+    // Allocate memory for another string
+    char *t = malloc(strlen(s) + 1);
+
+    // Copy string into memory
+    strcpy(t, s);
+
+    // Capitalize copy
+    t[0] = toupper(t[0]);
+
+    // Print strings
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+}
+
