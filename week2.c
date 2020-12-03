4d41482afa408ac4bd86b445cf0b5f1944849d95
Camilla <64920167+Camilla904@users.noreply.github.com>
2020-12-03 10:06:04 -0500

    Create week2.c

---
+++ week2.c
@@ -0,0 +1,268 @@
+// Printing characters in an array of strings
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    for (int i = 0; i < argc; i++)
+    {
+        for (int j = 0, n = strlen(argv[i]); j < n; j++)
+        {
+            printf("%c\n", argv[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Printing a command-line argument
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc == 2)
+    {
+        printf("hello, %s\n", argv[1]);
+    }
+    else
+    {
+        printf("hello, world\n");
+    }
+}
+
+// Returns explicit value from main
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("missing command-line argument\n");
+        return 1;
+    }
+    printf("hello, %s\n", argv[1]);
+    return 0;
+}
+
+// Prints ASCII codes
+
+#include <stdio.h>
+
+int main(void)
+{
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+    printf("%i %i %i\n", c1, c2, c3);
+}
+
+// Stores names using an array
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Names
+    string names[4];
+    names[0] = "EMMA";
+    names[1] = "RODRIGO";
+    names[2] = "BRIAN";
+    names[3] = "DAVID";
+
+    // Print Emma's name
+    printf("%s\n", names[0]);
+    printf("%c%c%c%c\n", names[0][0], names[0][1], names[0][2], names[0][3]);
+}
+
+// Averages three numbers using an array
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Scores
+    int scores[3];
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
+
+    // Print average
+    printf("Average: %i\n", (scores[0] + scores[1] + scores[2]) / 3);
+}
+
+// Averages numbers using a helper function
+
+#include <cs50.h>
+#include <stdio.h>
+
+float average(int length, int array[]);
+
+int main(void)
+{
+    // Get number of scores
+    int n = get_int("Scores:  ");
+
+    // Get scores
+    int scores[n];
+    for (int i = 0; i < n; i++)
+    {
+        scores[i] = get_int("Score %i: ", i + 1);
+    }
+
+    // Print average
+    printf("Average: %.1f\n", average(n, scores));
+}
+
+float average(int length, int array[])
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return (float) sum / (float) length;
+}
+
+// Prints string char by char, one per line
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string s = get_string("Input:  ");
+    printf("Output: ");
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        printf("%c", s[i]);
+    }
+    printf("\n");
+}
+
+// Prints string char by char, one per line, using strlen
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Input:  ");
+    printf("Output: ");
+    for (int i = 0; i < strlen(s); i++)
+    {
+        printf("%c", s[i]);
+    }
+    printf("\n");
+}
+
+// Prints string char by char, one per line, using strlen, remembering string's length
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Input: ");
+    printf("Output: ");
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c", s[i]);
+    }
+    printf("\n");
+}
+
+// Determines the length of a string
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt for user's name
+    string s = get_string("Name: ");
+
+    // Count number of characters up until '\0' (aka NUL)
+    int n = 0;
+    while (s[n] != '\0')
+    {
+        n++;
+    }
+    printf("%i\n", n);
+}
+
+// Uppercases a string
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Before: ");
+    printf("After:  ");
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            printf("%c", s[i] - 32);
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+    printf("\n");
+}
+
+// Uppercases string using ctype library (and an unnecessary condition)
+
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Before: ");
+    printf("After:  ");
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (islower(s[i]))
+        {
+            printf("%c", toupper(s[i]));
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+    printf("\n");
+}
+
+// Uppercases string using ctype library
+
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Before: ");
+    printf("After:  ");
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c", toupper(s[i]));
+    }
+    printf("\n");
+}
